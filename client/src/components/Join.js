bd6992eaaf0ebe71c84462ff7b579da7bacaa41d
abdo hassan <abdocoder0@gmail.com>
2020-04-13 00:23:13 +0200

    installing material ui

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,6 +1,50 @@
 import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Join = ({ history }) => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const Join = () => {
+  const classes = useStyles();
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
 
@@ -8,43 +52,54 @@ const Join = ({ history }) => {
     e.preventDefault();
     // history.push(`/chat?username=${username}&room=${room}`);
   };
-
   return (
-    <div className='join-container'>
-      <header className='join-header'>
-        <h1>
-          <i className='fas fa-code'></i> Chat Code
-        </h1>
-      </header>
-      <main className='join-main'>
-        <form onSubmit={handleSubmit}>
-          <div className='form-control'>
-            <label htmlFor='username'>Username</label>
-            <input
-              type='text'
-              name='username'
-              id='username'
-              placeholder='Enter username...'
+    <Grid container component='main' className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant='outlined'
+              margin='normal'
               required
+              fullWidth
+              id='email'
+              label='Email Address'
+              name='email'
+              autoComplete='email'
+              autoFocus
             />
-          </div>
-          <div className='form-control'>
-            <label htmlFor='room'>Room</label>
-            <select name='room' id='room'>
-              <option value='JavaScript'>JavaScript</option>
-              <option value='Python'>Python</option>
-              <option value='PHP'>PHP</option>
-              <option value='C#'>C#</option>
-              <option value='Ruby'>Ruby</option>
-              <option value='Java'>Java</option>
-            </select>
-          </div>
-          <button type='submit' className='btn'>
-            Join Chat
-          </button>
-        </form>
-      </main>
-    </div>
+            <TextField
+              variant='outlined'
+              margin='normal'
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              color='primary'
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
