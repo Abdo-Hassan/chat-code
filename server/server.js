05ac46e7f6a8370d270e3fde695ae48de8e54b68
abdo hassan <abdocoder0@gmail.com>
2020-06-25 23:40:49 +0200

    heroku production

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const path = require('path');
 // const { clearTyping, formatMessage } = require('./utils/messages');
 const formatMessage = require('./utils/messages');
 const {
@@ -21,6 +22,15 @@ const io = socketio(server);
 app.get('/', (req, res) => {
   res.send('Server is up and running');
 });
+
+// production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const botName = 'ChatCode Bot';
 
