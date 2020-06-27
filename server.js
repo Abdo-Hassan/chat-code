0c8b6d8911520793dc09acb13551c14ec02a9632
abdo hassan <abdocoder0@gmail.com>
2020-06-27 12:47:44 +0200

    edit

--- server.js
+++ server.js
@@ -106,4 +106,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`server runnigg on port ${PORT}`));
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
